ceb5a7842c1dab73ad9848ed8014ac903670e314
Tony904 <tnarde@gmail.com>
2021-06-23 15:40:45 -0400

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,10 +1,10 @@
 import os
 
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir("/darknet/data/obj"))
 for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".jpg", "JPG"):
-        image_files.append("data/obj/" + filename)
+    if filename.endswith(tuple([".jpg", "JPG"])):
+        image_files.append("/darknet/data/obj/" + filename)
 os.chdir("..")
 with open("train.txt", "w") as outfile:
     for image in image_files:
